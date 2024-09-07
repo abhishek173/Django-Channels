180d7c7ce31d64d1b949bd50a74a631d37055f69
abhishek173 <abhishekkumar801104@gmail.com>
2024-09-07 19:44:00 +0530

    front-end with websocket

---
+++ proj6/proj6/asgi.py
@@ -0,0 +1,19 @@
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj5.settings')
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import app.routing
+
+application = ProtocolTypeRouter({
+    'http' : get_asgi_application(),
+    'websocket' : URLRouter(
+        app.routing.websocket_patterns
+    )
+})
+
+
+
